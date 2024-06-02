0af3c7be1ca80e2f8bb6b7e6d264751c416c0e7a
Giladabudi6 <126001305+Giladabudi6@users.noreply.github.com>
2024-06-03 00:15:39 +0300

    Add files via upload

---
+++ שלב 1/selectAll.sql
@@ -0,0 +1,17 @@
+select comid, rank, numofunitcom
+from commander
+
+select DOCID, LICENSE_NUMBER, SENIORITY, NUMOFUNITDOC
+from doctor
+
+select PARID, LICENSE_NUMBER, SENIORITY, NUMOFUNITPAR
+from paramedic
+
+select EQUID, EXPERTISE, NUMOFUNITEQI
+from EQUIPMENT_MANAGER
+
+select LICENSE_NUMBER, DRID, NUMOFUNITDRI
+from driver
+
+select MEDID, SENIORITY, NUMOFUNITMED
+from medic
